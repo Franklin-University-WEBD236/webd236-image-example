f096c77bdcd3a204c3193f132cb9665c87c19de6
Glitch (tart-cemetery) <none>
2019-11-05 23:00:35 +0000

    📯🔦 Checkpoint ./index.php:6805087/181

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
   include_once('parts/utils.php');
   include_once('parts/db.php');
 
+$maxFun = function myMax($current, $new) {
+    return $current < $new ? $new : $current;
+}
 
+$arr = array(10, 5, 3, 5, 1, 2, 5, 7, 4);
+
+function reduce($arr, $maxFun) {
+  
+  
+}
 
 php include('parts/footer.php'); ?>
