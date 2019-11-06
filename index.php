52c1b7fb0da8f230b497919d6b1d65932e579bcb
Glitch (tart-cemetery) <none>
2019-11-06 23:31:38 +0000

    🚖🍏 Checkpoint ./index.php:6805087/565

--- index.php
+++ index.php
@@ -3,6 +3,19 @@
   include_once('parts/utils.php');
   include_once('parts/db.php');
 
+function myMax($current, $new) {
+    return $current < $new ? $new : $current;
+}
+
+$arr = array(10, 5, 3, 5, 1, 2, 5, 7, 4);
+
+function reduce($arr, $func) {
+  //over my head
+}
+
+print("Max: " . reduce($arr, 'myMax') . "<br />");
+
+
 class Car {
   private $fuel;
   private $miles;
