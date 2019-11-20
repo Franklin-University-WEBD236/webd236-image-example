564adbaca9dbd2936a963b0db3dc0c97be3330aa
Glitch (tart-cemetery) <none>
2019-11-20 01:18:36 +0000

    🍃👻 Checkpoint ./index.php:4738601/330

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
   
-
-$d = dir("/tmp/private/");
+$path = "/tmp/private/";
+$d = dir($path);
 while(false !== ($entry = $d->read())){
-  echo "<p>" . $entry . "</p>";
+  if($entry != "." && $entry != ".."){
+    $data = base64_encode(file_get_contents($path . $entry));
+    echo "<p><img src='data:image/jpeg;base64," . $data . "' style='width: 400px;' /></p>";
+  }
 }
