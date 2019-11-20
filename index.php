edfeb673dd719c95cb705c8638abbac13dc25a90
Glitch (tart-cemetery) <none>
2019-11-20 00:20:45 +0000

    🏆🙉 Checkpoint ./php.ini:4738601/4 ./index.php:4738601/240

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
-mail("tylerjwhitney@gmail.com", "Test Message", "This is where the message goes.");
+mail("tylerjwhitney@gmail.com", "Test Message", "This is where the message goes.",
+     "From: tyler@oafproductions.com, Reply-To: tyler.whitney@franklin.edu");
+
+echo "Tried to send that email.";
